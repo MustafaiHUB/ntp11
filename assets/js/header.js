5425e5b39e5ddb5e59bf36a3b1324859a8d2400d
pumpkinpies <ziadmhmh@gmail.com>
2024-03-02 18:50:20 +0300

    Had to add /ntp11 to all files to accomadate how github pages access resources. This can easily be reverted.

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,7 +5,7 @@ const headerHTML = `
 <div class="container">
 <a href="/index.html">
 <img
-  src="/assets/images/University_of_Jordan_Logo.png"
+  src="/ntp11/assets/images/University_of_Jordan_Logo.png"
   class="logo"
   alt="Logo"
 />
